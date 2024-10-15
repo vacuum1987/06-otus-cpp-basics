fb884dff7762b81dbeed74641d6cc7c28e0219b1
Dmitry <vacuum.rnd@gmail.com>
2024-10-15 16:40:12 +0300

    v1.3
    redundant memory allocation issue was fixed

--- dm_vector.hpp
+++ dm_vector.hpp
@@ -34,40 +34,16 @@ class Dm_vector {
         };
         
         void push_back (const T new_element) {
-            new_size = length + 1;
-            capacity_check_and_increase ();
-            T * new_region =  new T [new_size]; // new tmp region in memory
-            for (size_t i = 0; i < length; ++i) {
-                new_region[i] = values[i]; // copy elements from the old array
-            };
-            new_region[length] = std::move(new_element); // add new element as the last element of array
-            //delete [] values; // delete old array (old region)
-            values = new_region; // save new region values as array values
-            length += 1; // length updating 
-            // delete new_region;
+            
+            capacity_check_and_add (new_element, length);
+
         };
 
         // inserting new element into the array at the target  position
         void insert (const T new_element, int target_position ) {
-            new_size = length + 1;
-            capacity_check_and_increase ();
-            T * new_region =  new T [new_size]; // new tmp region in memory
-            // copy elements before target position
-            for (size_t i = 0; i < target_position; ++i) {
-                new_region[i] = values[i]; // copy elements from the old array
-            };
 
-            // copy elements after target position
-            for (size_t i = target_position; i < length; ++i) {
-                new_region[i+1] = values[i]; // copy elements from the old array
-            }; 
-
-            // add new element to target position 
-            new_region[target_position] = std::move(new_element);
-
-            values = new_region; // save new region values as array values
-            length += 1; // length updating 
-
+            capacity_check_and_add (new_element, target_position);
+            
         };
 
         void erase (int target_position) {
@@ -104,16 +80,42 @@ class Dm_vector {
        
         int length = 0;     // number of elements in the array
         int capacity = 0;   // capacity of the array (allocated memory)
-        int new_size = 0;     // tmp array size during insert and push_back operations
+        //int new_size = 0;     // tmp array size during insert and push_back operations
         T * values = new T [length];
 
         // internal function to check and increase array capacity in case of lower than (or equal to) new_size
-        void capacity_check_and_increase () {
+        void capacity_check_and_add (const T new_element, int target_position) {
+            int new_size = length + 1;
             if (capacity <= new_size) {
                 capacity = new_size * 1.5;
-                //std::cout << "Array capacity was increased. New capacity = " << capacity << std::endl;
                 new_size = capacity;
+                T * new_region =  new T [new_size]; // new tmp region in memory
+                // copy elements before target position
+                for (size_t i = 0; i < target_position; ++i) {
+                   new_region[i] = values[i]; // copy elements from the old array
+                };
+                
+                // add new element to target position 
+                new_region[target_position] = std::move(new_element);
+
+                // copy elements after target position
+                for (size_t i = target_position; i < length; ++i) {
+                    new_region[i+1] = values[i]; // copy elements from the old array
+                };      
+                        
+                values = new_region; // save new region values as array values
+                
+                //std::cout << "Array capacity was increased. New capacity = " << capacity << std::endl;
+                
                 //std::cout << " New size = " << new_size << std::endl;
+            }
+            else {
+                
+                for (size_t i = length; i > target_position; --i) {
+                    values[i] = values[i-1]; // move all elements to the next to position until target reached  
+                }; 
+                values[target_position] = std::move(new_element);     
             };
+            length += 1; // length updating
         };
 };
